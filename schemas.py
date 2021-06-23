ca1875d5cdd1907549cc0769bc4118d868f54757
Костюков Максим <enginer385@gmail.com>
2021-06-23 16:06:27 +1000

    Fixed user type

--- schemas.py
+++ schemas.py
@@ -58,7 +58,7 @@ class UserForSearchAnswer(BaseModel):
     email: Optional[str]
     account_image: Optional[str]
     subscribed: Optional[bool]
-    status: Optional[models.UserType]
+    type: Optional[models.UserType]
 
     class Config:
         orm_mode = True
