3420595198feb0045297d8acc136421af35b454d
Костюков Максим <enginer385@gmail.com>
2021-06-24 23:50:49 +1000

    Fixing briefs

--- articles.py
+++ articles.py
@@ -101,7 +101,7 @@ async def send_briefs_to_subscribed_users(
     text = "Check our new exciting news\n"
     for article in articles:
         text += (
-            f'<a href="https://news.asap-it.tech/{article.id}">{article.header}</a>\n'
+            f'<a href="https://news.asap-it.tech/{article.id}">{article.header}</a><br/>'
         )
     send_briefs(emails_for_sending, text)
     return {"result": "success"}
